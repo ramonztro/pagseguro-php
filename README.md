fcc735a7cb113400a0afc495f2065638977fad09
Ramon Kayo <contato@ramonkayo.com>
2016-06-17 14:02:48 -0300

    1.0.0

---
+++ README.md
@@ -0,0 +1,70 @@
+# PagSeguro-PHP *(API não oficial)*
+
+O PHP PagSeguro permite que você utilize a API de pagamentos, a API de notificações e 
+a API de transações do PagSeguro com facilidade. 
+
+## Guia Rápido
+
+### Instalando
+
+Instale a última versão com o comando:
+
+```bash
+$ composer require ramonztro/pagseguro-php
+```
+
+### Utilização
+
+```php
+<?php
+
+use Ramonztro\PhpPagSeguro\PagSeguro;
+
+//Cria objeto de comunicação com a API
+//Mais sobre o token: https://pagseguro.uol.com.br/v2/guia-de-integracao/como-comecar.html#!configure-seu-token
+$pagseguro = new PagSeguro('seu-email', 'seu-token');
+
+//Define os parâmetros da requisição de checkout
+//Veja mais em: https://pagseguro.uol.com.br/v2/guia-de-integracao/api-de-pagamentos.html
+$parametros = array(
+	'currency' => 'BRL',
+	'itemQuantity1' => '1',
+	'itemId1' => "id do produto",
+	'itemDescription1' => "título/descrição do produto",
+	'itemAmount1' => "100.00"
+);
+
+//Requisita um código de checkout
+$checkoutCode = $pagSeguro->checkoutCode($parametros);
+
+```
+
+## Sobre
+
+### Requisitos
+
+- PHP 5.3 ou mais recente.
+
+### Licença (MIT License)
+
+*Permission is hereby granted, free of charge, to any person obtaining a copy * 
+of this software and associated documentation files (the "Software"), to    
+deal in the Software without restriction, including without limitation the  
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+sell copies of the Software, and to permit persons to whom the Software is  
+furnished to do so, subject to the following conditions:*                    
+                                                                            
+*The above copyright notice and this permission notice shall be included in  
+all copies or substantial portions of the Software.*                         
+                                                                            
+*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS * 
+IN THE SOFTWARE.*
+
+### Autor
+
+Ramon Kayo - <contato@ramonkayo.com> - <http://twitter.com/ramonztro>
---
+++ src/PagSeguro.php
@@ -0,0 +1,143 @@
+<?php
+/*
++---------------------------------------------------------------------------+
+| PagSeguroPhp                                                              |
+| Copyright (c) 2013-2016, Ramon Kayo                                       |
++---------------------------------------------------------------------------+
+| Author        : Ramon Kayo                                                |
+| Email         : contato@ramonkayo.com                                     |
+| License       : Distributed under the MIT License                         |
+| Full license  : https://github.com/ramonztro/php-pagseguro/               |
++---------------------------------------------------------------------------+
+| "Simplicity is the ultimate sophistication." - Leonardo Da Vinci          |
++---------------------------------------------------------------------------+
+*/
+namespace Ramonztro\PagSeguroPhp;
+
+use \Exception;
+
+class PagSeguro {
+	
+	const 
+		UrlCheckout = 'https://ws.pagseguro.uol.com.br/v2/checkout/',
+		UrlTransactions = 'https://ws.pagseguro.uol.com.br/v3/transactions/',
+		UrlNotifications = 'https://ws.pagseguro.uol.com.br/v3/transactions/notifications/';
+	
+	private
+		$email = null,
+		$token = null;
+	
+	public function __construct($email, $token) {
+		if (!is_string($email)) throw new Exception('Email inválido.');
+		$email =  strtolower(trim($email));
+		$regex = '~^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$~';
+		if (!preg_match($regex, $email)) throw new Exception('Email inválido.');
+		
+		$this->email = $email;
+		
+		if (!is_string($token)) throw new Exception('Token inválido.');
+		$regex = '~^[a-zA-Z0-9]{32}$~';
+		if (!preg_match($regex, $token)) throw new Exception('Token inválido.');
+		
+		$this->token = $token;
+		
+		libxml_use_internal_errors(true);
+	}
+	
+	public function checkoutCode($parametros, $url = null) {
+		$xml = simplexml_load_string($this->checkout($parametros, $url));
+		if (!$xml)
+			throw new Exception('Problema ao ler arquivo XML.');
+		if (!is_null($xml->error[0]))
+			throw new Exception("{$xml->error[0]->message[0]} ({$xml->error[0]->code[0]})");
+		return $xml->code[0]->__toString();
+	}
+	
+	
+	public function checkout($parametros, $url = null) {
+		$url = is_null($url) ? self::UrlCheckout : $url;
+		
+		$parametrosPadrao['email'] = $this->email;
+		$parametrosPadrao['token'] = $this->token;
+		$parametros = array_merge($parametrosPadrao, $parametros);
+		
+		$postFields = '';
+		foreach($parametros as $chave => $valor) $postFields .= "$chave=$valor&";
+		$postFields = rtrim($postFields, '&');
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded; charset=ISO-8859-1'));
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, count($parametros));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+		$resposta = curl_exec($ch);
+		curl_close($ch);
+		return $resposta;
+	}
+	
+	public function transactionStatus($transaction, $url = null) {
+		$xml = simplexml_load_string($this->transaction($transaction, $url));
+		if (!$xml)
+			throw new Exception('Problema ao ler arquivo XML.');
+		if (!is_null($xml->error[0]))
+			throw new Exception("{$xml->error[0]->message[0]} ({$xml->error[0]->code[0]})");
+		return $xml->status[0]->__toString();
+	}
+	
+	public function transaction($transaction, $url = null) {
+		$url = is_null($url) ? self::UrlTransactions : $url;
+		$url .= $transaction;
+		$url .= "?email={$this->email}";
+		$url .= "&token={$this->token}";
+	
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded; charset=ISO-8859-1'));
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$resposta = curl_exec($ch);
+		curl_close($ch);
+	
+		return $resposta;
+	}
+
+	public function notificationStatus($notification, $url = null) {
+		$xml = simplexml_load_string($this->notification($notification, $url));
+		if (!$xml)
+			throw new Exception('Problema ao ler arquivo XML.');
+		if (!is_null($xml->error[0]))
+			throw new Exception("{$xml->error[0]->message[0]} ({$xml->error[0]->code[0]})");
+		return $xml->status[0]->__toString();
+	}
+	
+	public function notification($notification, $url = null) {
+		$url = is_null($url) ? self::UrlNotifications : $url;
+		$url .= $notification;
+		$url .= "?email={$this->email}";
+		$url .= "&token={$this->token}";
+	
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded; charset=ISO-8859-1'));
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$resposta = curl_exec($ch);
+		curl_close($ch);
+	
+		return $resposta;
+	}
+	
+	public function statusSignificado($codigo) {
+		switch ($codigo) {
+			case 1:	return "Aguardando pagamento: o comprador iniciou a transação, mas até o momento o PagSeguro não recebeu nenhuma informação sobre o pagamento.";
+			case 2:	return "Em análise: o comprador optou por pagar com um cartão de crédito e o PagSeguro está analisando o risco da transação.";
+			case 3:	return "Paga: a transação foi paga pelo comprador e o PagSeguro já recebeu uma confirmação da instituição financeira responsável pelo processamento.";
+			case 4:	return "Disponível: a transação foi paga e chegou ao final de seu prazo de liberação sem ter sido retornada e sem que haja nenhuma disputa aberta.";
+			case 5:	return "Em disputa: o comprador, dentro do prazo de liberação da transação, abriu uma disputa.";
+			case 6:	return "Devolvida: o valor da transação foi devolvido para o comprador.";
+			case 7:	return "Cancelada: a transação foi cancelada sem ter sido finalizada.";
+			case 8:	return "Chargeback debitado: o valor da transação foi devolvido para o comprador.";
+			case 9:	return "Em contestação: o comprador abriu uma solicitação de chargeback junto à operadora do cartão de crédito.";
+		}
+	}
+	
+}
